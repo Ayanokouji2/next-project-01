62d396708bc5c2c8a8b1dfbea5094991feb7ddff
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-06-30 21:58:21 +0530

    chore: Update DataFromToken to return NextResponse instead of object

--- src/helpers/DataFromToken.ts
+++ src/helpers/DataFromToken.ts
@@ -1,22 +1,34 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import jwt from 'jsonwebtoken'
 
 const SECRET = process.env.SECRET_TOKEN as string | undefined
 
-export function DataFromToken(req: NextRequest): { _id: string, email: string } | null {
+export function DataFromToken(req: NextRequest) : NextResponse{
 
     try {
         const token = req.cookies.get('token')?.value as string | ""
 
         if (!token) {
-            return null
+            return NextResponse.json({
+                success: false,
+                error: 'No token found',
+                status: 401
+            })
         }
 
         const user =  jwt.verify(token, SECRET as string ) as { _id : string, email : string }
 
-        return user
+        return NextResponse.json({
+            status: 200,
+            success: true,
+            user
+        })
     } catch (error: unknown) {
         console.log((error as Error).message)
-        return null
+        return NextResponse.json({
+            success: false,
+            error: (error as Error).message,
+            status: 500
+        })
     }
 }
